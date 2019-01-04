fce64c518047ca99a7cf3566baf797ba02ec7f16
Troy Johnson <troy.johnson@berkadia.com>
2019-01-04 14:11:44 -0700

    Added git ignore

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Welcome from './Welcome';
+import Secured from './Secured';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="container">
+          <ul>
+            <li><Link to="/">public component</Link></li>
+            <li><Link to="/secured">secured component</Link></li>
+          </ul>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/secured" component={Secured} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
-
 export default App;
